dda86e27dc280d7ad76f563e4dd38b007f2572bb
Brad Spurrell <63411329+bspur11@users.noreply.github.com>
2021-02-28 16:21:46 -0500

    Images2.

--- server.js
+++ server.js
@@ -1,22 +1,22 @@
-const express = require('express');
-const mysql = require('mysql2');
+// const express = require('express');
+// const mysql = require('mysql2');
 
-const PORT = process.env.PORT || 3001;
-const app = express();
-const apiRoutes = require('./routes/apiRoutes');
+// const PORT = process.env.PORT || 3001;
+// const app = express();
+// const apiRoutes = require('./routes/apiRoutes');
 
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
-app.use('/api', apiRoutes);
+// app.use(express.urlencoded({ extended: false }));
+// app.use(express.json());
+// app.use('/api', apiRoutes);
 
-// Default response for any other request (Not Found)
-app.use((req, res) => {
-  res.status(404).end();
-});
+// // Default response for any other request (Not Found)
+// app.use((req, res) => {
+//   res.status(404).end();
+// });
 
-// Start server after DB connection
-db.on('open', () => {
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-  });
-});
+// // Start server after DB connection
+// db.on('open', () => {
+//   app.listen(PORT, () => {
+//     console.log(`Server running on port ${PORT}`);
+//   });
+// });
